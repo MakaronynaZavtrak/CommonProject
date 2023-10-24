4e2909f02ec366da15cba44c6bd86f14a122975d
ElectricGtr <114348027+MakaronynaZavtrak@users.noreply.github.com>
2023-10-24 12:05:33 +0300

    Update Main.java

--- src/main/java/org/urfu/Main.java
+++ src/main/java/org/urfu/Main.java
@@ -4,13 +4,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean isOn = true;
-        Scanner inp = new Scanner(System.in);
-    ConsoleBot bot = new ConsoleBot();
-    bot.sayInfo();
-    while (isOn){
-        bot.write(inp.nextLine());
-    }
-    inp.close();
     }
 }
